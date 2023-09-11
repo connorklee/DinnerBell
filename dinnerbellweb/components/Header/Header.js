e6b2ad361fcafdf3a70e417c4dd055c0e2f06652
Connor Lee <connor.lee@yale.edu>
2023-09-11 11:07:55 +0800

    add header

--- dinnerbellweb/components/Header/Header.js
+++ dinnerbellweb/components/Header/Header.js
@@ -2,23 +2,24 @@
 
 import React, { useState, useEffect } from 'react'
 import Image from 'next/image';
+import { Tooltip, Button } from "@nextui-org/react";
 
 const Header = () => {
     return (
-        <div className='container-header flex justify-center items-center w-full h-24 p-b-5 border-b-2' >
-            <div className='alert cursor-pointer absoulte block border-2 p-2 rounded-3xl'>
-                <div style={{ width: '3em', height: '3em' }} className='mx-auto'>
-                    <Image
-                        src={"/alertbell.png"}
-                        alt={"alert"}
-                        width={100}
-                        height={100}
-                    />
+        <div className='container-header flex justify-center items-center w-full h-18 p-2 border-b-2' >
+            <Tooltip showArrow={true} placement='right' closeDelay={0} content="Ready to ring the dinner bell?">
+                <div className='alert cursor-pointer absoulte block border-2 p-2 rounded-3xl'>
+                    <div style={{ width: '2em', height: '2em' }} className='mx-auto'>
+                        <Image
+                            src={"/alertbell.png"}
+                            alt={"alert"}
+                            width={100}
+                            height={100}
+                        />
+                    </div>
+
                 </div>
-            </div>
-            <div>
-                Dinner Bell
-            </div>
+            </Tooltip>
         </div >
     )
 }
